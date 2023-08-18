65fec450eaef74622045930241cc7902b3ef81e7
happyphilosoph3r <happyphilosoph3r@gmail.com>
2023-08-18 15:04:51 +0100

    feat: add docker and all docker requirements to the project and update associated variables

--- student-attrition-server/utils/habitat.ts
+++ student-attrition-server/utils/habitat.ts
@@ -1,5 +1,5 @@
 export const PORT = process.env.PORT || 4001;
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const NODE_ENV = process.env.NODE_ENV || 'production';
 export const DOCKER = true;
-export const MODEL_IP = DOCKER ? 'model:3001' : '127.0.0.1:3001';
+export const MODEL_IP = DOCKER ? 'model:8000' : '127.0.0.1:3001';
 export const DATABASE_IP = DOCKER ? 'database' : '127.0.0.1:27017';
